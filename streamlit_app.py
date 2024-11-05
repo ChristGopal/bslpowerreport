4f2f891235fc58829513e7bd6a756c7a0deb157d
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 19:13:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ df[' Timestamp'] = pd.to_datetime(df[' Timestamp'])
 df = df.set_index(' Timestamp')
 weekly_sum = df[['DENT_MTR1','DENT_MTR2','DENT_MTR3','MDP01', 'MDP02', 'MDP03']].resample('M').sum()
 st.title("🎈 BSL Power Report apps")
-st.line_chart(weekly_sum)
+# st.line_chart(weekly_sum)
+st.bar_chart(weekly_sum)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
