392236dcc976a7f909fb6cf93da9402bf6af2be6
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 18:56:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
+df = pd.read_csv('Entegris_PowerCons.csv')
 st.title("🎈 My new BSL Power Report apps")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
