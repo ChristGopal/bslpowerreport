2ccf7529ebae4af85052e6b61e74fe59f8e1f869
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 20:04:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 st.title("🎈 BSL Power Report apps")
-st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).)"
+st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)")
 
