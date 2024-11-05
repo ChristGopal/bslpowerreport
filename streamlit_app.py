0c061e39f8f45311c02ede5a2a5d91cba5438f30
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 20:21:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,24 +9,16 @@ with col1:
     st.checkbox("Orient radio options horizontally", key="horizontal")
 
 with col2:
-    st.radio(
-        "Set label visibility 👇",
-        ["visible", "hidden", "collapsed"],
-        key="visibility",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        horizontal=st.session_state.horizontal,
+    Meter_Selection = st.radio(
+        "Select Your Meter Asset",
+        ["DENT_MTR1", "DENT_MTR2","DENT_MTR3","MDP01","MDP02","MDP03",],
+        index=None,
     )
-Meter_Selection = st.radio(
-    "Select Your Meter Asset",
-    ["DENT_MTR1", "DENT_MTR2","DENT_MTR3","MDP01","MDP02","MDP03",],
-    index=None,
-)
-Period_Selection = st.radio(
-    "Select Your Meter Asset",
-    ["D", "W","M","Y",],
-    index=None,
-)
+    Period_Selection = st.radio(
+        "Select Your Meter Asset",
+        ["D", "W","M","Y",],
+        index=None,
+    )
 
 st.write("You selected:", Meter_Selection, Period_Selection )
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)")
