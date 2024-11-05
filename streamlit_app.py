3604202707ad861601db7ecaa3e2b786c81f7dfa
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 19:24:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv('Entegris_PowerCons.csv')
 df[' Timestamp'] = pd.to_datetime(df[' Timestamp'])
 df = df.set_index(' Timestamp')
-weekly_sum = df[['DENT_MTR1','DENT_MTR2','DENT_MTR3','MDP01', 'MDP02', 'MDP03']].resample('M').sum()
+weekly_sum = df[['DENT_MTR1','DENT_MTR2','DENT_MTR3','MDP01', 'MDP02', 'MDP03']].resample('W').sum()
 st.title("🎈 BSL Power Report apps")
 # Melt the DataFrame to have a long format suitable for Streamlit
 weekly_sum_melted = pd.melt(weekly_sum.reset_index(), id_vars=[' Timestamp'], value_vars=['DENT_MTR1','DENT_MTR2','DENT_MTR3'], var_name='Power Meter', value_name='Total Consumption')
