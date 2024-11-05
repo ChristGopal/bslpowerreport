e7be4362ac9cd192178933ce5228034f8d0a2edc
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 20:02:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.title("🎈 BSL Power Report apps")
-meter_sel = st.radio(
-    ["DENT-MTR1", "DENT-MTR2","DENT-MTR3","MDP-MTR1","MDP-MTR2","MDP-MTR2",],
-    index=None,
-)
-period_sel = st.radio(
-    ["D", "W","M","Y",],
-    index=None,
-)
-st.write("You selected:", genre)
 
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 
