3edb94b5ec03967956e5fc81815dad03cbe82791
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 23:40:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with col2:
         index=1,
     )
 cons1 = df[Meter_Selection].resample(Period_Selection).sum()
-
+st.table(cons1)
 st.line_chart(cons1)
 st.write("You selected:", Meter_Selection, Period_Selection )
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)")
