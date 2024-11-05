d42291d8cbb2bd01434cfa202c3529eb86c0701c
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 19:03:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('Entegris_PowerCons.csv')
-st.title("🎈 My new BSL Power Report apps")
+df[' Timestamp'] = pd.to_datetime(df[' Timestamp'])
+df = df.set_index(' Timestamp')
+weekly_sum = df[['DENT_MTR1','DENT_MTR2','DENT_MTR3','MDP01', 'MDP02', 'MDP03']].resample('M').sum()
+st.title("🎈 BSL Power Report apps")
+st.line_chart(weekly_sum)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
