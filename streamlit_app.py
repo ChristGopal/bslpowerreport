99f43f1881e7bc9b1a9ca3029d4e308dc28dc069
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 20:05:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
 st.title("🎈 BSL Power Report apps")
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    index=None,
+)
+
+st.write("You selected:", genre)
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)")
 
