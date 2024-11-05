ffb89c4f61eff2345ce5a028df1a7e21f61a9fcd
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 23:11:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,7 @@ timestamp_column_name = ' Timestamp'
 df['Timestamp'] = pd.to_datetime(df[timestamp_column_name])
 df.set_index('Timestamp', inplace=True)
 column_names = df.columns.tolist()
- 
 col1, col2 = st.columns(2)
-
 with col1:
    Meter_Selection = st.radio(
         "Select Your Meter Asset",
@@ -23,7 +21,6 @@ with col2:
         ["D", "W","M","Y",],
         index=None,
     )
-
 st.write("You selected:", Meter_Selection, Period_Selection )
 st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)")
 
