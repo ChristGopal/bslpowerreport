d89862152f9f55a74c5e86908a070235c4802e33
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 20:20:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,21 @@ import streamlit as st
 import pandas as pd
 st.title("🎈 BSL Power Report apps")
 
+col1, col2 = st.columns(2)
+
 with col1:
     st.checkbox("Disable radio widget", key="disabled")
     st.checkbox("Orient radio options horizontally", key="horizontal")
+
+with col2:
+    st.radio(
+        "Set label visibility 👇",
+        ["visible", "hidden", "collapsed"],
+        key="visibility",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        horizontal=st.session_state.horizontal,
+    )
 Meter_Selection = st.radio(
     "Select Your Meter Asset",
     ["DENT_MTR1", "DENT_MTR2","DENT_MTR3","MDP01","MDP02","MDP03",],
