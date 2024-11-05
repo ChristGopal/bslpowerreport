39e04f04e6c8f8a334e54e9cc17b64b1e1a9ee3e
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-05 19:31:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ df = df.set_index(' Timestamp')
 weekly_sum = df[['DENT_MTR1','DENT_MTR2','DENT_MTR3','MDP01', 'MDP02', 'MDP03']].resample('W').sum()
 st.title("🎈 BSL Power Report apps")
 # Melt the DataFrame to have a long format suitable for Streamlit
-weekly_sum_melted = pd.melt(weekly_sum.reset_index(), id_vars=[' Timestamp'], value_vars=['DENT_MTR1','DENT_MTR2','DENT_MTR3'], var_name='Power Meter', value_name='Total Consumption')
+weekly_sum_melted = pd.melt(weekly_sum.reset_index(), id_vars=[' Timestamp'], value_vars=['DENT_MTR1'], var_name='Power Meter', value_name='Total Consumption')
 # Create the Streamlit bar chart
 st.area_chart(weekly_sum_melted, x=' Timestamp', y='Total Consumption', color='Power Meter')
 st.write(
