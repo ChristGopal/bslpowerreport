78d04b77befec25f4982e00b500303b1ebf70408
Gopal <73210658+ChristGopal@users.noreply.github.com>
2024-11-06 10:49:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ col1, col2 = st.columns(2)
 
 # Using "with" notation
 with st.sidebar:
-   st.logo("substation.gif")
+   st.logo("logo123.png")
    Meter_Selection = st.radio(
         "Select Your Meter Asset",
         [column_names[1],column_names[2],column_names[3],
